0ffe89675d3de13c860ab5b70c518e1b797aac5f
c0reac0de <candice.correa@gmail.com>
2018-05-22 15:42:40 -0400

    Update giphywork.js

--- giphywork.js
+++ giphywork.js
@@ -1,42 +1,78 @@
 //task 1: create array of topics
- var topics=["poppy","sunflower", "lavender","rose","dandelion"];
+var topics = ["poppy", "sunflower", "lavender", "rose", "dandelion"];
 
- //loads html first then js when ready
- $(document).ready(function(){
- //create for loop to display array items as buttons
- for (var i=0; i<topics.length; i++){
-     //creates new variable, new button div
-     var newFlower= $("<button>");
-     //adds bootstrap class to array items
-     newFlower.addClass('btn btn-primary');
-     //add a data attribute
-     newFlower.attr('data-name',topics[i], 'newFlower');
-      //adds topics array text to variable/button
-     newFlower.text(topics[i]);
-     //display array as buttons or add buttons to html
-     $('#flowers').append(newFlower);
- }
-
-
-//task 2: use ajax to connect to giphy api
-
- //Make the AJAX request to the API - GETs the JSON data at the queryURL.
- var queryURL="https://www.api.giphy.com/?q="+topics+"&flowers&api_key=BVqpweDs6Tt0YJUCYGiuPuNgi5uMH50h&limit=10";
- $.ajax({
- url:queryURL,
- method:'GET' })
-
- //task 3: store retrieved data in response object
- .then(function(response){
-     console.log(queryURL);
-   console.log(response);
- $("#flowers").html(response.topics[i]);
-
-//task 4:display retrieved data in specified
+//store giphy API URL for specified query params
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BVqpweDs6Tt0YJUCYGiuPuNgi5uMH50h&q="+topics+"&limit=10";
+//ajax request
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+})
+    //store retrieved data in response object
+    .then(function (response) {
+        //print entire object to console
+        console.log(response);
+        console.log(response.title);
+        //saving property of data in variable
+        var ajaxFlower = response;
+        // create new image div for new variable
+        var displayFlower = $("<img>");
+        //JSON stringify
+        $('#display-flowers').html(JSON.stringify(response));
+        //set new src for image variable
+        displayFlower.attr("src", ajaxFlower);
+        //set alternate attribute
+        displayFlower.attr("alt", "flower gif");
+        //append flowers to empty div
+        $("#display-flowers").append(displayFlower);
 
 
 
 
-});
- 
+
+
+    })
+//loads html first then js when ready
+$(document).ready(function () {
+    //create for loop to display array items as buttons
+    for (var i = 0; i < topics.length; i++) {
+        //creates new variable, new button div
+        var newFlower = $("<button>");
+        //adds bootstrap button class to array items
+        newFlower.addClass('btn btn-primary');
+        //add a data attribute
+        newFlower.attr('data-name', topics[i]);
+        //add css styling
+        newFlower.addClass('newFlower');
+        //adds topics array text to variable/button
+        newFlower.text(topics[i]);
+        //display array items as buttons  with added attributes
+        $('#flowers').append(newFlower);
+
+    }
+
+    //event handler-user input
+    $("#submit").on("click", function (event) {
+        // Preventing the buttons default behavior when clicked (which is submitting a form)
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var userFlower = $("#new-flower").val();
+        // Adding the movie from the textbox to our array
+        topics.push(userFlower);
+        console.log('the new flower is:' + ('#new-flower'));
+
+    });
+    //on user click of topic button, run following function
+    $("#flowers").on("click", function () {
+     // event.preventDefault() can be used to prevent an event's default behavior.
+      event.preventDefault();
+        //run query function
+        ajax(response);
+    })
+   
+
+    
+
+    //})
+
 });
